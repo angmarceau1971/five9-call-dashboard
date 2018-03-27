59b5f4b98fcd69fbc91530d79790a70dd743a3a8
kokopelli <nacclonts@gmail.com>
2018-03-27 13:22:41 -0600

    clear old Looker data when new data is pulled

--- src/datasources/controller.js
+++ src/datasources/controller.js
@@ -111,12 +111,19 @@ function getModelFromSourceName(sourceName) {
     }
 }
 
+/**
+ * @param  {String}  sourceName for datasource
+ * @return {Boolean} true if datasource is a custom upload or Looker data
+ */
 function isCustomSource(sourceName) {
     return getModelFromSourceName(sourceName) === custom.CustomData;
 }
 module.exports.isCustomSource = isCustomSource;
 
-
+/**
+ * Pulls data from @param model based on the given MongoDB @param aggregation
+ * pipeline.
+ */
 async function getStatisticsFrom(model, aggregation) {
     return new Promise((resolve, reject) => {
         model.aggregate(aggregation, (err, data) => {
@@ -167,21 +174,39 @@ function createGroup(groupBy, fields) {
 }
 
 
+/////////////////////////////////////////////////////////////////////////////
+// Looker-based custom data - Automatic Updating
+/////////////////////////////////////////////////////////////////////////////
+/**
+ * Begin updating Looker-based custom datasources every @param interval
+ * milliseconds.
+ * @param  {Number} interval in milliseconds
+ * @return {Number}          timeout ID for next occurrence
+ */
 async function scheduleLookerUpdates(interval) {
+    log.info(`Updating custom Looker data`);
     let authToken = await looker.getAuthToken();
     let sources = await custom.CustomDatasource.find({ fromLooker: true })
                                                 .lean().exec();
     for (let source of sources) {
         let raw = await looker.getJsonData(authToken, source.lookerLookId);
-        let data = formatLookerData(raw, source.lookerFieldLookup);
+        let data = formatFieldNames(raw, source.lookerFieldLookup);
         let clean = data.map(custom.rowParser(source));
+        let deleted = await clearOldData(source, clean);
         custom.CustomData.collection.insert(clean);
     }
-    return setTimeout(() => scheduleLookerUpdates(interval))
+    return setTimeout(() => scheduleLookerUpdates(interval), interval);
 }
 module.exports.scheduleLookerUpdates = scheduleLookerUpdates;
 
-function formatLookerData(data, fieldLookup) {
+/**
+ * Convert data fields from Looker to scorecard names.
+ * Any field names not included in @param fieldLookup will be excluded from output.
+ * @param  {Array} data        raw export from Looker
+ * @param  {Object} fieldLookup lookup for field names from custom datasource
+ * @return {Array} data with cleaned-up field names
+ */
+function formatFieldNames(data, fieldLookup) {
     return data.map((d) => {
         let res = {};
         for (let lookerField in fieldLookup) {
@@ -190,4 +215,32 @@ function formatLookerData(data, fieldLookup) {
         }
         return res;
     });
+}
+
+/**
+ * Delete data in @param datasource within *dates* that are including in the
+ * @param newData.
+ *
+ * @param  {Object} datasource
+ * @param  {Array} newData
+ * @return {Promise}
+ */
+async function clearOldData(datasource, newData) {
+    // find oldest and newest dates
+    let dates = newData.reduce((dates, d) => {
+        return {
+            min: Math.min(dates.min, d.date),
+            max: Math.max(dates.max, d.date)
+        };
+    }, { min: Infinity, max: -Infinity });
+    // delete former data
+    return await custom.CustomData.remove({
+        $and: [
+            { _datasourceName: datasource.name },
+            { date: {
+                $gte: new Date(dates.min),
+                $lte: new Date(dates.max)
+            } }
+        ]
+    })
 }
