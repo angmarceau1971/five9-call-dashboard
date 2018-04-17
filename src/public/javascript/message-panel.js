8c0b273ece580a5bff3bda17019ff0542e50b565
kokopelli <nacclonts@gmail.com>
2018-04-17 11:23:07 -0600

    put user-selector in message panel

--- src/public/javascript/message-panel.js
+++ src/public/javascript/message-panel.js
@@ -1,20 +1,37 @@
 import Vue from 'vue';
+import UsersSelector from '../components/users-selector.vue';
 import * as api from './api.js';
 
 const vm = new Vue({
     el: '#app',
 
+    components: {
+        'users-selector': UsersSelector
+    },
+
     data: {
         statusMessage: '',
         to: '',
         subject: '',
-        body: ''
+        body: '',
+        usersTo: []
+    },
+
+    computed: {
+        toNameList: function() {
+            return this.usersTo.reduce((str, user) =>
+                `${str ? str + ', ' : ''}${user.firstName} ${user.lastName}`
+            , '');
+        }
     },
 
     methods: {
+        selectUsers: function(users) {
+            this.usersTo = users;
+        },
         send: async function (toString, subject, body) {
-            let users = toString.split(',').map(username => {
-                return { username: username }
+            let users = this.usersTo.map(user => {
+                return { username: user.username }
             });
             let message = {
                 to: users,
