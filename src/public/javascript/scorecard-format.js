10abedc2796d5602f6d6be65a3c6fc7d455e235b
Nathan Clonts <nacclonts@gmail.com>
2018-06-12 17:03:05 -0600

    hotfix: make durations >1 hour use HH:mm:ss format

--- src/public/javascript/scorecard-format.js
+++ src/public/javascript/scorecard-format.js
@@ -9,6 +9,14 @@ let comparators = {
 };
 
 
+/**
+ * Returns a formatted value based on the given value and field.
+ * @param  {Any} value usually a numeric value
+ * @param  {String or Object} field name of field, or full Field object
+ * @param  {String} [skillGroup=null] skill group to apply goal coloring
+ * @return {Object}         object with keys 'value' and
+ *                          'styleClass' (CSS class name to apply)
+ */
 export function formatValue(value, field, skillGroup=null) {
     let formattedValue;
     if (typeof(field) == 'string') {
@@ -23,16 +31,28 @@ export function formatValue(value, field, skillGroup=null) {
 
     let style = getStyleFromGoal(value, field, skillGroup);
 
+    ///////////////////// Number values
     if (isNumberType(field.format.type)) {
         if (isNaN(value) || !isFinite(value)) formattedValue = 'N/A';
         else formattedValue = d3.format(field.format.string)(value);
+    ///////////////////// Durations
     } else if (field.format.type == 'Duration') {
-        if (isNaN(value) || !isFinite(value)) formattedValue = 'N/A';
-        else formattedValue = moment('2018-01-01').startOf('day')
-            .seconds(value)
-            .format(field.format.string);
+        if (isNaN(value) || !isFinite(value)) {
+            formattedValue = 'N/A';
+        } else {
+            let formatString = field.format.string;
+            // For times over 1 hour, include hours
+            if (value > 3599 && formatString == 'm:ss') {
+                formatString = 'H:mm:ss';
+            }
+            formattedValue = moment('2018-01-01').startOf('day')
+                .seconds(value)
+                .format(formatString);
+        }
+    ///////////////////// Time / Date
     } else if (field.format.type == 'Time') {
         formattedValue = moment(value).format(field.format.string);
+    ///////////////////// Default to raw value
     } else {
         formattedValue = value;
     }
