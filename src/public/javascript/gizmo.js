c7422b9044bb3da70950223afa2d1469ab33fc3d
Nathan Clonts <nacclonts@gmail.com>
2019-08-10 17:06:07 -0600

    Queues: Skill Group selector

--- src/public/javascript/gizmo.js
+++ src/public/javascript/gizmo.js
@@ -7,6 +7,8 @@ export default function GizmoManager() {
     this.gizmos = null;
     // which gizmo has a menu open?
     this.openGizmoId = null;
+    // skill groups and associated skills
+    this.skillGroups = [];
 
     this.build = function(id=null) {
         let template = document.getElementById('gizmo-template');
@@ -27,13 +29,14 @@ export default function GizmoManager() {
         if (!this.gizmos[id]) {
             this.gizmos[id] = {
                 name: 'New one!',
-                skillFilter: []
+                skillFilter: [],
+                useSkillGroupFilter: true,
+                skillGroupFilter: [],
             }
         }
         // set name for DOM
         $(gizmo).find('.department-name').html(this.gizmos[id].name);
 
-        this.setupInteractions(id);
         return id;
     }
 
@@ -45,28 +48,76 @@ export default function GizmoManager() {
 
     // Which gizmo is currently edited in the skill menu? This function will
     // update that gizmo's attributes.
-    this.updateCurrent = function(name, skills) {
+    this.updateCurrent = function(name, skills, skillGroups, useSkillGroupFilter) {
         this.gizmos[this.openGizmoId].name        = name;
         this.gizmos[this.openGizmoId].skillFilter = skillStringToArray(skills);
+        this.gizmos[this.openGizmoId].useSkillGroupFilter = useSkillGroupFilter;
+        if (useSkillGroupFilter) {
+            this.gizmos[this.openGizmoId].skillGroupFilter = [skillGroups];
+        } else {
+            this.gizmos[this.openGizmoId].skillGroupFilter = [];
+        }
     }
 
-    // Set up menu interactions for a gizmo with the given ID
+    // Set up DOM and menu interactions for gizmo with the given ID
     this.setupInteractions = function(id) {
         let gizmo = $('#' + id);
+        let selectedGizmo = this.gizmos[id];
 
-        // Skills menu
+        // Open skills modal
         gizmo.find('.skills-edit-toggle').click(function (event) {
             // Show the modal...
             $('.modal').css('display', 'block');
             // Track currently open menu...
             this.openGizmoId = id;
             // And set modal values to match this gizmo
-            $('.modal').find('.gizmo-name').val(this.gizmos[id].name);
-            $('.modal').find('.skills').val(this.gizmos[id].skillFilter);
+            $('.modal').find('.gizmo-name').val(selectedGizmo.name);
+            $('.modal').find('.skills').val(selectedGizmo.skillFilter);
+
+            // use Skill Groups UI if needed
+            if (selectedGizmo.useSkillGroupFilter) {
+                $('.modal').find('#skill-filter-group-radio').prop('checked', true);
+                $('.modal').find('.skills').attr('disabled', true);
+                $('.modal').find('.skill-groups').removeAttr('disabled');
+            } else {
+                $('.modal').find('#skill-filter-list-radio').prop('checked', true);
+                $('.modal').find('.skill-groups').attr('disabled', true);
+                $('.modal').find('.skills').removeAttr('disabled');
+            }
+            // Skill list/group radio buttons
+            $("input[name='skill-filter-type']").change(function (e) {
+                if ($(this).val() == 'skill-group') {
+                    $('.modal').find('.skills').attr('disabled', true);
+                    $('.modal').find('.skill-groups').removeAttr('disabled');
+                } else {
+                    $('.modal').find('.skill-groups').attr('disabled', true);
+                    $('.modal').find('.skills').removeAttr('disabled');
+                }
+            });
+
+            // build skill group select list
+            let selectedSkillGroup = (selectedGizmo.skillGroupFilter || []).length ? selectedGizmo.skillGroupFilter[0] : null
+            let skillGroupSelect = $('.modal').find('.skill-groups').empty();
+            function makeOption(val, text) {
+                skillGroupSelect.append($(
+                    `<option ${val === selectedSkillGroup ? 'selected' : '' }></option>`
+                ).attr('value', val).text(text));
+            };
+            makeOption('null_perspiration', 'Select a skill group');
+            for (let skillGroup of this.skillGroups) {
+                makeOption(skillGroup.name, skillGroup.name);
+            }
+
+            // Sync Skills List text with selected skill group
+            skillGroupSelect.on('change', function(e) {
+                let name = e.target.value;
+                let newGroup = this.skillGroups.find((group) => group.name === name);
+                $('.modal').find('.skills').val(newGroup.skills)
+            }.bind(this));
         }.bind(this));
 
         // Show/hide queue list
-        this.gizmos[id].showQueueList = false;
+        selectedGizmo.showQueueList = false;
         gizmo.find('.show-skills-list').click(function (event) {
             gizmo.find('.show-skills-list').toggleClass('selected');
             gizmo.find('.queue-list').toggleClass('hidden');
@@ -76,11 +127,12 @@ export default function GizmoManager() {
     // Get a new ID string in the form 'gizmo-<integer>'
     this.nextGizmoId = function() {
         if (!this.gizmos) return 'gizmo-0';
-        let lastId = Math.max(
-            ...Object.keys(this.gizmos).map((id) =>
-                id.split('-')[1]
-            )
-        );
+
+        let currentIdNumbers = Object.keys(this.gizmos)
+            .map((id) => id.split('-')[1])
+            .filter((num) => !isNaN(num));
+
+        let lastId = Math.max(...currentIdNumbers);
         return `gizmo-${lastId + 1}`;
     };
 
@@ -96,19 +148,38 @@ export default function GizmoManager() {
         if ($(event.target).is('.modal'))
             $('.modal').css('display', 'none');
     }.bind(this));
+
     // Listen for skill filter updates
     $('.modal .save').click(function updateSkillFilter() {
+        const isUsingSkillGroups = $('input[name=skill-filter-type]:checked').val() === 'skill-group';
         const name   = $('.modal .gizmo-name').val();
         const skills = $('.modal .skills').val();
+        const skillGroups = $('.modal .skill-groups').val();
+
         $('#' + this.openGizmoId).find('.department-name').html(name);
-        this.updateCurrent(name, skills);
+        this.updateCurrent(name, skills, skillGroups, isUsingSkillGroups);
         this.save();
     }.bind(this));
+
+    // On cancel, revert to last saved/cached state
+    $('.modal .cancel').click(function() {
+        this.load(false);
+    }.bind(this));
+
     // Listen for add-gizmo button
     $('.add-gizmo').click(function addGizmo() {
-        let newID = this.build();
+        let newId = this.build();
+        this.setupInteractions(newId);
         // save current state to local storage
         this.save();
+    }.bind(this));
+
+    $('.edit-buttons .reset-gizmos').click(async function() {
+        this.skillGroups = await api.getSkillGroups();
+        this.setupDefaultGizmos();
+        $('.gizmo').remove();
+        this.save();
+        this.load(true);
     }.bind(this));
 
 
@@ -118,21 +189,28 @@ export default function GizmoManager() {
         localStorage.setItem('user_gizmos', data);
     }
 
+    // Set up default gizmos from server's skill groups
+    this.setupDefaultGizmos = function() {
+        let i = 0;
+        this.gizmos = this.skillGroups.reduce((res, skillGroup) => {
+            res[`gizmo-${i++}`] = {
+                name: skillGroup.name,
+                queueList: [],
+                showQueueList: false,
+                skillFilter: skillGroup.skills,
+                useSkillGroupFilter: true,
+                skillGroupFilter: [skillGroup.name],
+            };
+            return res;
+        }, {});
+    }
+
     // Load gizmos from local storage on startup
-    this.load = async function() {
+    this.load = async function(rebuildDom) {
         let data = localStorage.getItem('user_gizmos');
+        this.skillGroups = await api.getSkillGroups();
         if (!data) {
-            let skillGroups = await api.getSkillGroups();
-            let i = 0;
-            this.gizmos = skillGroups.reduce((res, skillGroup) => {
-                res[`gizmo-${i++}`] = {
-                    name: skillGroup.name,
-                    queueList: [],
-                    showQueueList: false,
-                    skillFilter: skillGroup.skills
-                };
-                return res;
-            }, {});
+            this.setupDefaultGizmos();
             console.log('Loading default gizmos:', this.gizmos);
         } else {
             this.gizmos = JSON.parse(data);
@@ -140,7 +218,10 @@ export default function GizmoManager() {
         }
         // Build view
         for (const id of Object.keys(this.gizmos)) {
-            this.build(id);
+            if (rebuildDom) {
+                this.build(id);
+            }
+            this.setupInteractions(id);
         };
     }
 }
